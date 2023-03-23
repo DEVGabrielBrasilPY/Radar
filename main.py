8e5defa9ae109da5c80538fc8cdb7b5ba0b880c9
DEVGabrielBrasilPY <127884139+DEVGabrielBrasilPY@users.noreply.github.com>
2023-03-23 16:56:50 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+#condição simples(radar)
+velocidade = float(input('Qual é a velocidade atual do carro?'))
+if velocidade > 80:
+    print('MULTADO! Você excedeu o valor limite da via que é de 80 km/h')
+    multa = (velocidade-80) * 7
+    print('Você deve pagar uma multa de R${:.2f}!'.format(multa))
+print('Tenha um bom dia! Dirija com segurança!')
